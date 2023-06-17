11e61c4419a6a52afba343dff58e6c91f42ca029
Yassin Magdi Elsir <yme@MBP-von-Yassin.fritz.box>
2023-06-17 14:32:57 +0200

    ready for feature selection algorithm

--- lstm/code/lstm_interface.py
+++
@@ -1,9 +0,0 @@
-from lstm_data import X, y
-from lstm_functions import *
-
-
-lstm1 = lstm_init(X,y)
-
-data_predict, dataY_plot = lstm_predict(X,y)
-
-predictionsfigure(data_predict,dataY_plot)
--- xg/code/xg.py
+++ xg/code/xg.py
@@ -1,11 +1,19 @@
 import pandas as pd
-import numpy as np
-from xg_functions import categorizecols
+from xg_functions import *
+
 
 df = pd.read_csv('/Users/yme/code/AppliedAI/summativeassessment/data/full_dataset.csv').fillna(0)
 
 X, y = df[df.columns.difference(['ISO','Country','Coal'])], df[['Country','Year','Total','Coal']]
 
+
 categorizecols(X)
     
 
+
+
+
+xg_init(X, y)
+
+preds = xg_predict(X, y)
+predictionsfigure(preds, X, y)
