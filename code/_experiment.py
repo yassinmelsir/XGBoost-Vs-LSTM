7e2dc1ccfd92ebb51ade70be3268b7d6480f5081
Yassin Magdi Elsir <yme@MBP-von-Yassin.fritz.box>
2023-06-22 20:36:52 +0200

    finished

--- code/_experiment.py
+++ code/_experiment.py
@@ -1,14 +1,16 @@
 from analysis import *
 from figures import figure_one, figure_two
 
-# xg_best_features, lstm_best_features = fs(10) # feature selection
+xg_best_features, lstm_best_features = fs(10) # feature selection
 
-# xg_w_nofs() # xg with no feature selection
+features = [3,4,5]
 
-# lstm_w_nofs() # lstm with no feature selection
+xg_w_nofs() # xg with no feature selection
 
-# xg_w_fs(xg_best_features) # xg with feature selection
+lstm_w_nofs() # lstm with no feature selection
 
-# lstm_w_fs(lstm_best_features) # lstm with feature selection
+xg_w_fs(xg_best_features) # xg with feature selection
+
+lstm_w_fs(lstm_best_features) # lstm with feature selection
 
 figure_two()
--- code/functions.py
+++ code/functions.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
 
 def get_data(dataset):
-    # df = pd.read_csv('/Users/yme/code/AppliedAI/summativeassessment/data/full_dataset.csv').fillna(0)
-    df = lastsixtyyears()
+    df = pd.read_csv('/Users/yme/code/AppliedAI/summativeassessment/data/full_dataset.csv').fillna(0)
+    # df = extended_dataset()
     y = df[['Country','Year','Total']] 
     X = df.drop(columns=['ISO','Total']) 
     
@@ -36,78 +36,9 @@ def decode(df,y):
 
     return y
 
-# def multiple_runs_figure():
-#     predicted_year= preds['Year'].to_numpy()
-#     predicted_emissions = preds
-
-#     actual_year = input_X['Year'].to_numpy()
-#     actual_emissions = input_y['Total'].to_numpy()
-    
-#     plt.figure(figsize=(10,6))
-#     for pred in preds:
-
-#         predicted_year= X['Year'].to_numpy()
-#         predicted_emissions = pred
-#         plt.plot(predicted_year,predicted_emissions,label='Predicted')
-    
-
-
-#     plt.plot(actual_year,actual_emissions,label='Actual')
-#     plt.xlabel('Year')
-#     plt.ylabel('Emissions')
-#     plt.title('Actual vs Predicted Emissions per Year')
-
-#     #performance plot
-
-#     plt.legend()
-#     plt.show()
-
-# def performance_figure(preds, X, y):
-#     predicted_emissions = preds
-#     actual_emissions = y['Total'].to_numpy()
-    
-#     plt.figure(figsize=(10,6))
-#     plt.plot(predicted_emissions,label='Predicted')
-#     plt.plot(actual_emissions,label='Actual')
-#     plt.xlabel('Year')
-#     plt.ylabel('Emissions')
-#     plt.title('Actual vs Predicted Emissions per Year')
-#     plt.legend()
-#     plt.show()
-
-# def predictions_figure(preds, X, y):
-#     predicted_year= X['Year'].to_numpy()
-#     predicted_emissions = preds
-
-#     actual_year = X['Year'].to_numpy()
-#     actual_emissions = y['Total'].to_numpy()
-    
-#     plt.figure(figsize=(10,6))
-#     plt.plot(predicted_year,predicted_emissions,label='Predicted')
-#     plt.plot(actual_year,actual_emissions,label='Actual')
-#     plt.xlabel('Year')
-#     plt.ylabel('Emissions')
-#     plt.title('Actual vs Predicted Emissions per Year')
-#     plt.legend()
-#     plt.show()
-
-# def plus30years(df): 
-#     newDf = df[0:0].copy()
-#     countries = df['Country'].unique()
-#     for country in countries:
-#         oldCountry = df[(df['Country'] == country) & (df['Year'] >= 1990)].copy()
-#         newCountry = oldCountry[(oldCountry['Year'] >= 1990)].copy()
-#         newCountry['Year'] = newCountry['Year'] + 30
-        
-#         for column in newDf.columns.values:
-#             if (column != 'Country') & (column != 'Year'): newCountry[column] = float(0)
-#         newDfSlice = pd.concat([oldCountry,newCountry])
-#         newDf = pd.concat([newDf,newDfSlice]).sort_values(by=['Country', 'Year'], ascending=True)
-#     return newDf
-
-def lastsixtyyears(): 
+def extended_dataset(): 
     df = pd.read_csv('/Users/yme/code/AppliedAI/summativeassessment/data/full_dataset.csv').fillna(0)
-    newDf = df[0:0].copy()
+    newDf = df.copy()
     countries = df['Country'].unique()
     for country in countries:
         oldCountry = df[(df['Country'] == country) & (df['Year'] >= 1990)].copy()
@@ -116,36 +47,5 @@ def lastsixtyyears():
         for column in newDf.columns.values:
             if (column != 'Country') & (column != 'Year'): newCountry[column] = float(0)
         # newDfSlice = pd.concat([oldCountry,newCountry])
-        newDf = pd.concat([df,newCountry])
+        newDf = pd.concat([newDf,newCountry])
     return newDf
-
-# def next35Years(df): 
-#     newDf = df[0:0].copy()
-#     countries = df['Country'].unique()
-#     for country in countries:
-#         oldCountry = df[(df['Country'] == country) & (df['Year'] >= 1990)].copy()
-#         newCountry = oldCountry.copy()
-#         newCountry['Year'] = newCountry['Year'] + 35
-#         for column in newDf.columns.values:
-#             if (column != 'Country') & (column != 'Year'): newCountry[column] = float(0)
-#         newDf = pd.concat([newDf,newCountry])
-#     return newDf
-
-# def country(df, label): 
-#     country = df[((df['Country'] == label) & (df['Year'] >= 1990))]
-#     country['Year'] = country['Year'] + 32
-#     for column in df.columns.values:
-#         if (column != 'Country') & (column != 'Year'): country[column] = float(0)
-#     return country
-
-# def getxgdata():
-#     df = pd.read_csv('/Users/yme/code/AppliedAI/summativeassessment/data/full_dataset.csv').fillna(0)
-
-#     X, y = df.drop(columns=['ISO','Total']), df[['Country','Year','Total']]
-    
-#     categorizecols(X)
-#     return X, y
-
-# def categorizecols(X):
-#     cats = X.select_dtypes(exclude=np.number).columns.to_list()
-#     for col in cats: X[col] = X[col].astype('category')
